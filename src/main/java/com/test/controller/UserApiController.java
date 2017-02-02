97e475eae29a1abdad58e66c811e89a306cc11b2
EggUncle <cicadashadow@gmail.com>
2017-02-02 20:47:38 +0800

    添加客户端用户获取自己博客的接口

--- src/main/java/com/test/controller/UserApiController.java
+++ src/main/java/com/test/controller/UserApiController.java
@@ -1,15 +1,17 @@
 package com.test.controller;
 
+import com.test.model.BlogEntity;
+import com.test.model.BlogJson;
 import com.test.model.LoginJson;
 import com.test.model.UserEntity;
 import com.test.repository.BlogRepository;
 import com.test.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by egguncle on 17-1-24.
@@ -30,6 +32,7 @@ public class UserApiController {
 
     /**
      * 客户端进行登录的类
+     *
      * @param userName
      * @param passwd   此处的密码是在客户端经过MD5加密的值，加密过程在客户端执行
      * @return
@@ -58,5 +61,44 @@ public class UserApiController {
         }
 
     }
+
+    /**
+     * 获取对应用户的博客（20条）
+     *
+     * @param type   获取类型 one：获取指定ID的博客数据
+     *               max：获取比输入ID大的数据
+     *               min：获取比输入ID小的数据
+     *               max_user：获取用户比ID大的数据
+     *               min_user：获取用户比ID小的数据
+     * @param blogId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/api/blog/{type}/{userId}/{blogId}", method = RequestMethod.GET)
+    public BlogJson getBlogJson(@PathVariable("type") String type, @PathVariable("userId") int userId, @PathVariable("blogId") int blogId) {
+
+        List<BlogEntity> blogList = new ArrayList<>();
+
+        switch (type) {
+            case "one": {
+                blogList.add(blogRepository.findOne(blogId));
+            }
+            break;
+            case "max": {
+                blogList.addAll(blogRepository.getBlogListMaxWithUser(blogId, userId));
+            }
+            break;
+            case "min": {
+                blogList.addAll(blogRepository.getBlogListMinWithUser(blogId, userId));
+            }
+            break;
+        }
+        BlogJson blogJson = new BlogJson();
+        blogJson.setError(false);
+        blogJson.setResults(blogList);
+
+        return blogJson;
+    }
+
 
 }
